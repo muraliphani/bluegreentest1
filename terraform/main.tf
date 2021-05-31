99ad86603534f616079e3651e8762653c66c9a56
muraliphani <35369872+muraliphani@users.noreply.github.com>
2021-05-31 23:22:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  region     = "us-east-2"
-  access_key = "AKIATJ77KBHGQQ2N3OX7"
-  secret_key = "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"
-}
+
 
 resource "aws_instance" "web-server-instance"{
   ami           = "ami-01aab85a5e4a5a0fe"
